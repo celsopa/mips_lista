933e3728def61bedfccf420d2bce258265e0ee38
celsopa <celsopa@msn.com>
2019-11-19 00:10:40 -0300

    Implementado o menu e inicio da funcao adicionar elemento.

--- projeto_mips_lista.asm
+++ projeto_mips_lista.asm
@@ -7,14 +7,25 @@
 	prompt_del_indice: .asciiz "\nInforme o indice do elemento a ser deletado: "	
 	prompt_recupera_lista: .asciiz "\nA lista informada é: "
 	prompt_sair: .asciiz "\nEncerrando aplicação..."
-	erro_tamanho_lista_impossivel: .asciiz "\n[ERRO] Tamanho da lista inválido."
-	erro_tamanho_lista_maximo: .asciiz "\n[ERRO] Tamanho maximo da lista atingido."
-	erro_indice: .asciiz  "\n[ERRO] Índice acimo do limite."
+	prompt_lista_vazia: .asciiz "\nA lista não contém elementos."
+	msg_erro_tamanho_lista_impossivel: .asciiz "\n[ERRO] Tamanho da lista inválido."
+	msg_erro_menu_invalido: .asciiz "\n[ERRO] Opção inválida."
+	msg_erro_tamanho_lista_maximo: .asciiz "\n[ERRO] Tamanho maximo da lista atingido."
+	msg_erro_indice: .asciiz  "\n[ERRO] Índice acimo do limite."
   
 .text
 main:
-#Leitura do tamanho da lista
+#INICIO DO PROGRAMA
+
+#Carregamento dos dados iniciais
 	li $t1, 0 #tamanho atual da lista em $t1
+	li $s1, 1 #Adicionar elemento
+	li $s2, 2 #Recuperar elemento
+	li $s3, 3 #Imprimir lista
+	li $s4, 4 #Deletar elemento
+	li $s5, 5 #Sair
+
+#Leitura do tamanho da lista	
 	la $a0, prompt_tam_lista
 	li $v0, 4
 	syscall
@@ -22,10 +33,12 @@ main:
 	li $v0, 5
 	syscall
 	
-	blez $v0, erro_tam_lista_impossivel #Verifica se o tamanho da lista informado é válido ou não
+	blez $v0, erro_tamanho_lista_impossivel #Verifica se o tamanho da lista informado é válido ou não
 	
 	move $t0, $v0 #tamanho maximo da lista em $t0		
-		
+
+
+			
 inicio_menu:
 #Imprime o menu
 	la $a0, prompt_menu
@@ -35,6 +48,23 @@ inicio_menu:
 	la $a0, prompt_acao
 	li $v0, 4
 	syscall
+	
+	li $v0, 5
+	syscall
+	
+	blez $v0, erro_menu_invalido
+	bgt $v0, $s5, erro_menu_invalido
+	
+	beq $v0, $s1, func_adicionar_elemento #1 - Adicionar elemento
+	j inicio_menu
+	beq $v0, $s2, func_recuperar_elemento #2 - Recuperar elemento
+	j inicio_menu
+	beq $v0, $s3, func_imprimir_lista #3 - Imprimir lista
+	j inicio_menu
+	beq $v0, $s4, func_deletar_elemento #4 - Deletar elemento
+	j inicio_menu
+	beq $v0, $s5, func_sair #5 - Sair
+	j inicio_menu
 
 
 
@@ -42,31 +72,82 @@ inicio_menu:
 
 
 
+#############
+##  ERROS  ##
+#############
 
 #Erro - Tamanho da lista inválido
-erro_tam_lista_impossivel:
-	la $a0, erro_tamanho_lista_impossivel
+erro_tamanho_lista_impossivel:
+	la $a0, msg_erro_tamanho_lista_impossivel
 	li $v0, 4
 	syscall
 	j main
 
 #Erro - Tamanho da lista inválido
-erro_tam_lista_maximo:
-	la $a0, erro_tamanho_lista_maximo
+erro_menu_invalido:
+	la $a0, msg_erro_menu_invalido
 	li $v0, 4
 	syscall
 	j inicio_menu
 
+#Erro - Tamanho da lista inválido
+erro_tamanho_lista_maximo:
+	la $a0, msg_erro_tamanho_lista_maximo
+	li $v0, 4
+	syscall
+	j inicio_menu
 
-func_novo_elemento:
-	sle $t5, $t1, $t0
-	blez $t5, erro_tam_lista_maximo
+###############
+##  FUNÇÕES  ##
+###############
+
+## FUNÇÃO ADICIONAR ELEMENTO ##
+func_adicionar_elemento:
+#Checa se é possível adicionar novo elemento na lista
+	sle $t5, $t1, $t0 #Se Tamanho atual da lista é menor que o tamanho maximo  $t5 recebe 1
+	blez $t5, erro_tamanho_lista_maximo  #Se $t5 <= 0 pula para 'erro_tam_lista_maximo' 
 	
+#Leitura do novo elemento da lista
 	la $a0, prompt_novo_elemento
+	li $v0, 4
+	syscall
+	
 	li $v0, 5
 	syscall
 	
+#Armazena o valor lido na stack
 	add $sp, $sp, -4
 	sw $v0, 0($sp)
 	
+#Incrementa o tamanho da lista
 	addi $t0, $t0,1
+	
+	
+	
+	
+	
+	
+	
+	
+
+## FUNÇÃO RECUPERAR ELEMENTO ##
+func_recuperar_elemento:
+
+
+## FUNÇÃO IMPRIMIR LISTA ##
+func_imprimir_lista:
+
+
+## FUNÇÃO DELETAR ELEMENTO ##
+func_deletar_elemento:
+
+
+
+
+
+func_sair:
+	la $a0, prompt_sair
+	li $v0, 4
+	syscall
+	li $v0, 10
+	syscall
